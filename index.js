c04984f1a73b129bf2c9e3d844741d8abbd85e51
eruleman <eruleman@mit.edu>
2015-01-05 17:56:00 -0500

    Update index.js
    Trying to create a table in Postgresql database on AWS.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var pg = require("pg");
 var express = require('express')
 var app = express();
 
@@ -11,3 +12,22 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+var conString = "pg://OutfitrDBUser:OutfitrDBPassword@outfitrdb.cdqciw4zlodp.us-west-2.rds.amazonaws.com:5432/OutfitrDB";
+
+var client = new pg.Client(conString);
+client.connect();
+
+client.query("CREATE TABLE IF NOT EXISTS emps(firstname varchar(64), lastname varchar(64))");
+client.query("INSERT INTO emps(firstname, lastname) values($1, $2)", ['Ronald', 'McDonald']);
+client.query("INSERT INTO emps(firstname, lastname) values($1, $2)", ['Mayor', 'McCheese']);
+
+var query = client.query("SELECT firstname, lastname FROM emps ORDER BY lastname, firstname");
+query.on("row", function (row, result) {
+    result.addRow(row);
+});
+query.on("end", function (result) {
+    console.log(JSON.stringify(result.rows, null, "    "));
+    client.end();
+});
+
