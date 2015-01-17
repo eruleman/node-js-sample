a3b9dc5a1e241ed4e8ea2c1096d23d688f9dcd41
eruleman <eruleman@mit.edu>
2015-01-17 17:07:56 +0100

    Added Amazon SDK for NodeJS
    Also added a function to send an AWS SQS message.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+var AWS = require('aws-sdk');
 var pg = require("pg");
 var express = require('express')
 var app = express();
+
+// set the region for AWS API requests
+AWS.config.region = 'us-west-2';
 
 app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
@@ -19,9 +23,32 @@ app.get('/get-next-top-id', function(request, response) {
   response.send("" + Math.floor((Math.random() * 9)));
 })
 
+app.get('/send-sqs-message', function(request, response) {
+  sendSQSMessage(request.query.messagebody);
+  response.send("SQS Message sent! Message body: " + request.query.messagebody);
+})
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+// Configure an Amazon SQS service.
+var sqs = new AWS.SQS();
+var params = {
+  AWSAccountIds: [ /* required */
+    '2753-3335-6355',
+  ],
+  Actions: [ /* required */
+    'SendMessage',
+    'GetQueueAttributes',
+  ],
+  Label: 'NodeJSSendMessage', /* required */
+  QueueUrl: 'https://sqs.us-west-2.amazonaws.com/275333356355/GenerateNextArticleMLQueue' /* required */
+};
+sqs.addPermission(params, function (err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     console.log(data);           // successful response
+});
 
 var conString = "pg://OutfitrDBUser:OutfitrDBPassword@outfitrdb.cdqciw4zlodp.us-west-2.rds.amazonaws.com:5432/OutfitrDB";
 var client = new pg.Client(conString);
@@ -37,4 +64,20 @@ function createUser(username, password, firstname, lastname) {
     console.log(JSON.stringify(result.rows, null, "   "));
     client.end();
   })
+}
+
+function sendSQSMessage(messageBody) {
+  var params = {
+    MessageBody: messageBody, /* required */
+    QueueUrl: 'https://sqs.us-west-2.amazonaws.com/275333356355/GenerateNextArticleMLQueue', /* required */
+    DelaySeconds: 0,
+  };
+  sqs.sendMessage(params, function(err, data) {
+    if (err) {
+      console.log(err, err.stack); // an error occurred
+    }
+    else {
+      console.log(data);           // successful response
+    }
+  });
 }
